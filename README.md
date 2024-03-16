f3f668269f84c9bd5fbcdc14b6e2a3544d895a2a
TheAVieira <149870014+TheAVieira@users.noreply.github.com>
2024-03-16 23:35:36 +0200

    Initial website
    Initial website with HTML5UP template

---
+++ README.md
@@ -0,0 +1,18 @@
+My porfolio and some thoughts and dreams.
+
+
+---
+
+
+
+Story by HTML5 UP
+Creator of the static website template: | AJ | aj@lkn.io | @ajlkn
+On html5up.net
+Licensed under the CCA 3.0 (html5up.net/license)
+
+Other Credits:
+	Icons: Font Awesome (fontawesome.io)
+	Other:
+		jQuery (jquery.com)
+		Scrollex (github.com/ajlkn/jquery.scrollex)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
---
+++ assets/js/imageflipper.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('.imageflipperFade').each(function() {
+        var $this = $(this);
+        var $imgs = $this.find('img');
+        var $imgCount = $imgs.length;
+        var $currentImgIndex = 0;
+        var interval;
+
+        // Function to start the animation
+        function startAnimation() {
+            $imgs.hide().eq($currentImgIndex).show(); // Ensure current image is shown when animation restarts
+            interval = setInterval(function() {
+                $imgs.eq($currentImgIndex).fadeOut(200, function() {
+                    $currentImgIndex = ($currentImgIndex + 1) % $imgCount;
+                    $imgs.hide().eq($currentImgIndex).fadeIn(200);
+                });
+            }, 4000); // Adjust time as needed
+        }
+
+        // Function to stop the animation
+        function stopAnimation() {
+            clearInterval(interval);
+        }
+
+        // Initially start the animation
+        startAnimation();
+
+        // Use hover to stop/start the animation on mouse enter/leave
+        $this.hover(
+            function() { // Mouse enters the element
+                stopAnimation();
+            }, 
+            function() { // Mouse leaves the element
+                startAnimation();
+            }
+        );
+    });
+});
+
+$(document).ready(function(){
+    $('.imageflipper').each(function() {
+        var $this = $(this);
+        var $imgs = $this.find('img');
+        var $imgCount = $imgs.length;
+        var $currentImgIndex = 0;
+
+        $imgs.hide().first().show(); // Hide all images, then show the first
+
+        setInterval(function() {
+            $imgs.eq($currentImgIndex).hide();
+            $currentImgIndex = ($currentImgIndex + 1) % $imgCount;
+            $imgs.eq($currentImgIndex).show();
+        }, 4000); // Change image every 6 seconds for each span separately
+    });
+});
